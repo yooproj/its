ed6bb71f71c3845f7819af618ba5231c2a1873da
Anna Borzenko <annnechko@gmail.com>
2024-06-14 18:52:11 +1200

    delete next install react

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,126 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {KeplerGl} from "@kepler.gl/components";
+import {connect, ReactReduxContext} from 'react-redux'
+import {addDataToMap} from '@kepler.gl/actions';
+import {replaceDataInMap} from "kepler.gl/src/actions";
+import {ProtoDataset} from "@kepler.gl/types";
+import {ReplaceDataToMapOptions} from "@kepler.gl/actions/dist/actions";
+import {ReplaceDataInMapPayload} from "kepler.gl/src/actions/dist/actions";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component<any, any> {
+  componentDidMount() {
+
+
+
+    const sampleTripData = {
+      fields: [
+        {name: 'tpep_pickup_datetime', format: 'YYYY-M-D H:m:s', type: 'timestamp'},
+        {name: 'pickup_longitude', format: '', type: 'real'},
+        {name: 'pickup_latitude', format: '', type: 'real'}
+      ],
+      rows: [
+        [
+          ['2015-01-15 19:05:39 +00:00',  174.76219,-36.848450]
+        ]
+      ]
+    };
+    const eventSource = new EventSource('//localhost:3000/sse');
+    eventSource.onmessage = ({ data }) => {
+
+      const sampleTripData2 = {
+        fields: [
+          {name: 'tpep_pickup_datetime', format: 'YYYY-M-D H:m:s', type: 'timestamp'},
+          {name: 'pickup_longitude', format: '', type: 'real'},
+          {name: 'pickup_latitude', format: '', type: 'real'}
+        ],
+        rows: JSON.parse(data)
+      };
+      console.log(data)
+      this.props.dispatch(
+        replaceDataInMap({
+          datasetToReplaceId: 'test_trip_data',
+          datasetToUse: {
+            info: {
+              label: 'Sample Taxi Trips in New York City',
+              id: 'test_trip_data'
+            },
+            data: sampleTripData2
+          },
+          options:{
+            centerMap: false
+          }
+
+        } as ReplaceDataInMapPayload))
+    }
+    const sampleConfig = {
+      // visState: {
+        // filters: [
+        //   {
+        //     id: 'me',
+        //     dataId: 'test_trip_data',
+        //     name: 'tpep_pickup_datetime',
+        //     type: 'timeRange',
+        //     view: 'enlarged',
+        //     value: ''
+        //   }
+        // ]
+      // }
+    }
+
+
+    // setTimeout(() => {
+    //   debugger
+    this.props.dispatch(
+      addDataToMap(
+        {
+          datasets: {
+            info: {
+              label: 'Sample Taxi Trips in New York City',
+              id: 'test_trip_data'
+            },
+            data: sampleTripData
+          },
+          options: {
+            centerMap: true,
+            readOnly: false,
+            keepExistingConfig: false
+          },
+          info: {
+            title: 'Taro and Blue',
+            description: 'This is my map'
+          },
+          config: sampleConfig
+        }
+      ))
+  }
+
+  render() {
+
+
+    const token: string = 'pk.eyJ1IjoibmVrb3phZW1vbiIsImEiOiJjbHc3bXVhOGUxaDZ0MmtxZXJlaG5uODV2In0.r-32TJBbgyDT2kfgpHBfFg'
+    return <>
+      <div>
+        <h1>Start Screen</h1>
+        <ReactReduxContext.Consumer>
+          {({store}) => (
+            <div className="App">
+              <KeplerGl
+                id="foo"
+                mapboxApiAccessToken={token}
+                store={store}
+                height={600}
+                width={1200}
+              />
+            </div>
+          )}
+        </ReactReduxContext.Consumer>
+      </div>
+    </>;
+  }
+
 }
 
-export default App;
+const mapStateToProps = state => state;
+const dispatchToProps = dispatch => ({dispatch});
+export default connect(mapStateToProps, dispatchToProps)(App);
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import keplerGlReducer from "@kepler.gl/reducers";
+import {taskMiddleware} from "react-palm/tasks";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const reducer = combineReducers({
+  // <-- mount kepler.gl reducer in your app
+  keplerGl: keplerGlReducer,
+
+});
+const store = createStore(reducer, {}, applyMiddleware(taskMiddleware));
+
 root.render(
-  <React.StrictMode>
+
+  <Provider store={store}>
+
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
