cb6592c4e0cf68b4553d7178feb3419efd3c7236
Anna Borzenko <annnechko@gmail.com>
2024-06-29 22:12:00 +1200

    add real time toggle

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -12,12 +12,12 @@ import {ParsedConfig} from "@kepler.gl/types/schemas";
 class App extends Component<any, any> {
   private alerts_fields: any;
   private veh_fields: any;
-  private readonly config: ParsedConfig;
+  private readonly config: any;
 
   constructor(props) {
     super(props);
     const DATE_FORMAT = 'h:mm a MMMM DD, YYYY'
-
+    this.state = {off: false}
     this.alerts_fields = []
     this.veh_fields = []
     this.config = {
@@ -97,9 +97,10 @@ class App extends Component<any, any> {
             options: {
               centerMap: false,
               readOnly: false,
-              keepExistingConfig: false
+              autoCreateLayers: true,
+              keepExistingConfig: true
             },
-            config: this.config,
+            // config: this.config,
             info: {
               title: 'Auckland Transport',
               description: 'Auckland Transport'
@@ -120,6 +121,7 @@ class App extends Component<any, any> {
           options: {
             centerMap: false,
             autoCreateLayers: false,
+            keepExistingConfig: true,
           }
 
         } as ReplaceDataInMapPayload))
@@ -136,6 +138,9 @@ class App extends Component<any, any> {
       this.setData(data.data, true)
       const eventSource = new EventSource('//localhost:3000/sse');
       eventSource.onmessage = ({data}) => {
+        if (this.state.off === true) {
+          return;
+        }
         const response_data = JSON.parse(data)
         this.setData(response_data)
       }
@@ -143,11 +148,36 @@ class App extends Component<any, any> {
   }
 
   render() {
+    const updateOff = (e) => {
+      this.setState({off: document.querySelector('#off')['checked'] === true})
+    }
 
     const token: string = 'pk.eyJ1IjoibmVrb3phZW1vbiIsImEiOiJjbHc3bXVhOGUxaDZ0MmtxZXJlaG5uODV2In0.r-32TJBbgyDT2kfgpHBfFg'
     return <>
       <div>
-        <h1>Start Screen</h1>
+        <div className={'header'}>
+          <h1>Auckland Public Transport</h1>
+          <div className="header-real">Real-time update:</div>
+          <div className="normal-container">
+            <div className="smile-rating-container">
+              <div className="smile-rating-toggle-container">
+                <form className="submit-rating">
+                  <input id="off" name="satisfaction" type="radio" onClick={updateOff}/>
+                  <input id="on" name="satisfaction" type="radio" defaultChecked={true} onClick={updateOff}/>
+                  <label htmlFor="off" className="rating-label rating-label-off">Off</label>
+                  <div className="smile-rating-toggle"></div>
+
+                  <div className="rating-eye rating-eye-left"></div>
+                  <div className="rating-eye rating-eye-right"></div>
+                  <div className="mouth rating-eye-bad-mouth"></div>
+
+                  <div className="toggle-rating-pill"></div>
+                  <label htmlFor="on" className="rating-label rating-label-on">On</label>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
 
         <ReactReduxContext.Consumer>
           {({store}) => (
@@ -157,7 +187,7 @@ class App extends Component<any, any> {
                 mapboxApiAccessToken={token}
                 store={store}
                 height={600}
-                width={1200}
+                width={1300}
               />
             </div>
           )}
